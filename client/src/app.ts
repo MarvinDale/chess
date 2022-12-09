b647d2b7d3b7c906e6529cc471e387b2369119b7
Marvin Dale <marvinfrancisdale@gmail.com>
2022-12-09 21:05:45 +0000

    populate board using typescript

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,7 +1,28 @@
-const board: HTMLElement = document.getElementById("board")!;
+const boardElement: HTMLElement = document.getElementById("board")!;
+
+// prettier-ignore
+const boardCoordinates: string[] = [
+  "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+  "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+  "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+  "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+  "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+  "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+  "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+  "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+]
 
 let originalXPos: string = "0";
 let originalYPos: string = "0";
+
+window.onload = () => {
+  for (let i = 0; i < boardCoordinates.length; i++) {
+    let square = document.createElement("div");
+    square.className = "tile";
+    square.innerHTML = `<div id="${boardCoordinates[i]}" class="piece white-pawn"></div>`;
+    boardElement.appendChild(square);
+  }
+};
 
 window.addEventListener("load", () => {
   let pieces = document.getElementsByClassName(
@@ -11,7 +32,7 @@ window.addEventListener("load", () => {
   originalYPos = pieces.item(0)!.style.top;
 });
 
-board.addEventListener("mousedown", (event: MouseEvent) => {
+boardElement.addEventListener("mousedown", (event: MouseEvent) => {
   let target = event?.target as HTMLElement;
   console.log(target.id);
   if (target.id != "board") {
